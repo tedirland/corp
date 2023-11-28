c3e7b7177035e0cd3bcf5ffd2984f18e16b76b51
tedirland <irlandth@gmail.com>
2023-11-28 07:58:58 -0600

    render image to home screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,19 @@
 import Link from "next/link"
+import Image from "next/image"
+import homeImg from "public/images/home.jpg"
+
 export default function Home() {
   return (
-   <div>
-    <h1>Home Page</h1>
-   </div>
+    <div className="absolute -z-10 inset-0">
+  <Image
+  alt="car factory"
+  src={homeImg}
+  fill
+  style={{objectFit: 'cover'}}
+  />
+  </div>
+
+  
+
   )
 }
